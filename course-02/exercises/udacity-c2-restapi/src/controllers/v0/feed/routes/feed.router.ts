e356c60cd687c629e98e1f12b84e6f3d0fca2030
Ryan Riley <ryan.b.riley@colorado.edu>
2019-12-16 15:44:37 -0500

    add get and patch endpoints

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,13 +18,42 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+    const item = await FeedItem.findByPk(req.params.id);
+    if(item.url) {
+        item.url = AWS.getGetSignedUrl(item.url);
+    }
+res.send(item);
+
+})
+
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
+        const id = req.params.id
+        const caption = req.body.caption;
+        const fileName = req.body.url;
+    
+        // check Caption is valid
+        if (!caption && !fileName) {
+            return res.status(400).send({ message: 'Caption or filename must be changed' });
+        }
+        
+        const item = await FeedItem.findByPk(id);
+        if(caption) {
+            item.caption = caption;
+        }
+        if(fileName) {
+            item.url = fileName;
+        }
+    
+        const saved_item = await item.save();
+    
+        saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+        res.status(201).send(saved_item);
 });
 
 
